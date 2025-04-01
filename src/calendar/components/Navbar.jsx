4780d23fc2193d390b25f8bd49a8a9cc0e695502
cpandares <pandaresocesar@gmail.com>
2025-03-31 20:29:02 -0400

    Eliminar importación innecesaria de useSelector en Navbar.jsx

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { logout } from "../../store";
 import { useAuthStore } from "../../hooks";
 
